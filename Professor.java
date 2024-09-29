1733eca86eb9ecdaec30126747b91a947cea45a5
Felipssv <161666209+Felipssv@users.noreply.github.com>
2024-09-29 11:57:21 -0300

    Create Professor.java

---
+++ Professor.java
@@ -0,0 +1,17 @@
+package projetoBiblioteca;
+
+public class Professor extends Usuario {
+    public static final int max_reservas = 8;
+
+    public Professor(String nome){
+        super(nome);
+    }
+
+    @Override
+    public void reservarItem(Item item) throws ItemIndisponivelException{
+        if(getItensReservados().size() >= max_reservas){
+            throw new ItemIndisponivelException("O professor já atingiu o número máximo de reservas");
+        }
+        super.reservarItem(item);
+    }
+}
