7e24c0826ec202ce8f6a1979a81153ccd163396d
Felipssv <161666209+Felipssv@users.noreply.github.com>
2024-09-29 11:57:38 -0300

    Create Biblioteca.java

---
+++ Biblioteca.java
@@ -0,0 +1,45 @@
+package projetoBiblioteca;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private List<Item> acervo;
+    private List<Usuario> usuarios;
+    
+    public Biblioteca(){
+        this.acervo = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+    }
+
+    public void adicionarItem(Item item){
+        acervo.add(item);
+    }
+
+    public void adicionarUsuario(Usuario usuario){
+        usuarios.add(usuario);
+    }
+
+    public void reservarItem(Usuario usuario, String titulo){
+        for (Item item : acervo){
+            if (item.getTitulo() == (titulo)){
+                try{
+                    usuario.reservarItem(item);  
+                    return;
+                } catch(ItemIndisponivelException e){
+                    System.out.println("O item não está disponível! " + e.getMessage());
+                    return;
+                }
+            }
+        }
+        System.out.println("Item não encontrado!");
+    }
+    public void devolverItem(Usuario usuario, String titulo){
+        for(Item item : usuario.getItensReservados()){
+            if (item.getTitulo() == (titulo)){
+                usuario.devolverItem(item);
+                return;
+            }
+        }
+        System.out.printf("O item %s não está reservado", titulo);
+    }
+}
