298872944bade1b017e6f19410960e7fe3c5a9eb
Felipssv <161666209+Felipssv@users.noreply.github.com>
2024-09-29 11:57:56 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+package projetoBiblioteca;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+
+        Livro livro1 = new Livro("O Senhor dos Anéis", "J.R.R Tolken", "Happer Collins Brasil");
+        Livro livro2 = new Livro("1984", "George Orwell", "Companhia das letras");
+        Revista revista1 = new Revista("Veja 2", "Veja");
+
+        biblioteca.adicionarItem(livro1);
+        biblioteca.adicionarItem(livro2);
+        biblioteca.adicionarItem(revista1);
+
+        // Criando usuários
+        Aluno aluno1 = new Aluno("Jorge");
+        Professor professor1 = new Professor("Dr. Leôncio");
+
+        biblioteca.adicionarUsuario(aluno1);
+        biblioteca.adicionarUsuario(professor1);
+
+        // Reservando itens
+        biblioteca.reservarItem(aluno1, "1984");
+        biblioteca.reservarItem(professor1, "O Senhor dos Anéis");
+
+        // Tentativa de emprestar item indisponível
+        biblioteca.reservarItem(aluno1, "O Senhor dos Anéis");
+
+        // Devolvendo item
+        biblioteca.devolverItem(professor1, "O Senhor dos Anéis");
+
+        // Tentativa de devolver um item não emprestado
+        biblioteca.devolverItem(aluno1, "O Senhor dos Anéis");
+    }
+}
